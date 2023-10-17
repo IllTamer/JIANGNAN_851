367ec45ffc3d3d4ab630b32db1ada342dbd5c813
IllTamer <78360471+IllTamer@users.noreply.github.com>
2023-10-17 17:57:11 +0800

    Feat: Update impl types

--- algorithm/02/02_ChangeProblem.c
+++ algorithm/02/02_ChangeProblem.c
@@ -1,42 +1,68 @@
 #include "../../main.h"
 
-// 找零问题（此实现为dp版，贪心版不限制硬币数量，从大减到小就完事了
-// https://www.jianshu.com/p/d7cf201ecb72
+#define INF (1e9+7)
+int min(int a, int b) {
+    return a < b ? a : b;
+}
+
+// 找零问题
+// https://blog.csdn.net/Cowry5/article/details/100992653
 
 // 需找零金额为M,给一些不同面值的硬币<a_{0},a_{1}.....a_{n}>，
 //      要计算出找M所需要的最少硬币数
-// dp[i] = dp(dp[i-coin] + 1) (i >= c)
+// dp[i] = min(dp[i], dp[i-coin]+1) (i >= c)
+
 /**
+ * 完全背包版（每个硬币可选无限次
  * @param amount 金额
- * @param coins 硬币数组，默认升序，默认coins[0]=1
+ * @param coins 硬币数组，默认升序
  * @param coinsNum 硬币种类数 / 硬币数组长度
  * */
  // 时间复杂度 O(mn)，空间复杂度 O(m)
-int ChangeProblem(int amount, const int coins[], int coinsNum) {
+int ChangeProblem_Total(int amount, const int coins[], int coinsNum) {
     // dp[i] 表示凑金额i需要花费的硬币数
     int dp[amount + 1] = {};
-
-    for (int i = 1; i <= amount; ++i) {
-        // 初始化最少用到的硬币数，为 i/coins[0]
-        // 一般题中 coins[0]=1，故初始时 min = i/1 = i
-        int min = i;
-        for (int j = 0; j < coinsNum; ++ j) {
-            int coin = coins[j];
-            if (i >= coin) {
-                // 当前金额最少找零数量 = (前金额-硬币面值)的最少找零数量 + 1
-                int minTemp = dp[i-coin] + 1;
-                // 更新最小找零数，并在所有硬币枚举完后记入 dp[]
-                if (minTemp < min) min = minTemp;
-            }
-        }
-        dp[i] = min;
-    }
-    return dp[amount];
+    // 初始化数组，若结果是INF,则找不到目标数量
+    for (int i = 1; i <= amount; ++ i) dp[i] = INF;
+    // 从小到大遍历coin
+     for (int coinIndex = 0; coinIndex < coinsNum; ++ coinIndex) {
+         int coin = coins[coinIndex];
+         // 从小到大计算dp[i] —— **重复选取硬币**
+         // 从coin面值起计算找零 (i-coin >= 0)
+         for (int i = coin; i <= amount; ++ i) {
+             dp[i] = min(dp[i], dp[i-coin]+1);
+         }
+     }
+    return dp[amount] == INF ? -1 : dp[amount];
 }
 
+/**
+ * 01背包版（每个硬币最多选一次
+ * @param amount 金额
+ * @param coins 硬币数组，默认升序
+ * @param coinsNum 硬币种类数 / 硬币数组长度
+ * */
+// 时间复杂度 O(mn)，空间复杂度 O(m)
+int ChangeProblem_01(int amount, const int coins[], int coinsNum) {
+    int dp[amount + 1] = {};
+    for (int i = 1; i <= amount; ++ i) dp[i] = INF;
+
+    for (int coinIndex = 0; coinIndex < coinsNum; ++ coinIndex) {
+        int coin = coins[coinIndex];
+        // 从大到小计算dp[i] —— **硬币仅选取一次**
+        for (int i = amount; i >= coin; -- i) {
+            dp[i] = min(dp[i], dp[i-coin]+1);
+        }
+    }
+    return dp[amount] == INF ? -1 : dp[amount];
+}
+
+// 多重背包版（每个硬币数量有限
+// TODO 超纲，理应不考
+
 int main() {
-    int value = 63;
-    int coins[] = {1,5,10,25};
-    int min = ChangeProblem(value, coins, sizeof(coins)/sizeof(coins[0]));
+    int value = 7;
+    int coins[] = {2,5,10,25};
+    int min = ChangeProblem_01(value, coins, sizeof(coins)/sizeof(coins[0]));
     printf("Min count: %d", min);
 }
